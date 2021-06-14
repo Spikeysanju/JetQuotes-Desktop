fb413b48f458acd7e27892490cc3ce370cc4eed9
Spikeysanju <spikeysanju98@gmail.com>
2021-06-14 09:48:16 +0530

    🧹 Remove unused functions
    Signed-off-by: Spikeysanju <spikeysanju98@gmail.com>

--- src/main/kotlin/viewmodel/MainViewModel.kt
+++ src/main/kotlin/viewmodel/MainViewModel.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import model.QuoteItem
 import utils.ViewState
 import java.io.File
 
@@ -15,20 +14,13 @@ class MainViewModel {
 
     // Backing property to avoid state updates from other classes
     private val _uiState = MutableStateFlow<ViewState>(ViewState.Loading)
-    private val _search = MutableStateFlow<String>("")
-
 
     // UI collects from this StateFlow to get it's state update
     val uiState = _uiState.asStateFlow()
-    val search = _search.asStateFlow()
 
     private lateinit var viewModelScope: CoroutineScope
     fun init(scope: CoroutineScope) {
         this.viewModelScope = scope
-    }
-
-    fun inputValue(text: String) = viewModelScope.launch(Dispatchers.IO) {
-        _search.value = text
     }
 
     /**
@@ -56,33 +48,4 @@ class MainViewModel {
             _uiState.value = ViewState.Error(e.message ?: "Something went wrong")
         }
     }
-
-    /**
-     * To get all quotes from Json
-     */
-    fun searchQuotes(text: String) = viewModelScope.launch(Dispatchers.IO) {
-        /* filePath */
-        val quotes = "/Users/spikeysanju/JetQuotes/src/main/resources/assets/quotes.json"
-
-        /* reading json file */
-        val quotesJson = File(quotes).readText()
-
-        /* format json */
-        val format = Json {
-            ignoreUnknownKeys = true
-            prettyPrint = true
-            isLenient = true
-        }
-
-        /* decode from string (JSON) */
-        try {
-            _uiState.value = ViewState.Success(
-                format.decodeFromString<List<QuoteItem>>(quotesJson).filter { it.quoteText.contains(text) })
-        } catch (e: Exception) {
-            e.printStackTrace()
-            _uiState.value = ViewState.Error(e.message ?: "Something went wrong")
-        }
-    }
-
-
 }
